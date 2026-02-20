b617f586f52b4db842b5199ec7dfdafe6aa7294c
Thejan Bandara <hmagthejanbandar@gmail.com>
2026-02-20 22:02:18 +0530

    chore: Comment out various JavaScript code examples including calculations, lists, objects, functions, and JSON.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
 // console.log(age);    
 
 // calculate 
+/* 
 let a = 10
 let b = 20
 let c = 30
@@ -24,4 +25,98 @@ let g = 70
 let h = 80
 let i = 90
 let j = 100
-console.log(a + b + c + d + e + f + g + h + i + j)
+console.log(a + b + c + d + e + f + g + h + i + j) */
+
+// list
+/*  let number = ["hello", "world", "javascript"]
+ console.log(number)    
+ number [0] = "hii"
+ console.log(number) */
+ 
+// object
+/* let person = {
+    name: "John",
+    age: 20,
+    city: "New York"
+}
+console.log(person) */
+
+// function
+
+/* function maketea (){
+     
+    console.log("boil water")
+    console.log("put tea in water")
+    console.log("pour tea in cup")
+    console.log("add sugar")
+    console.log("serve tea")
+    console.log("enjoy your tea")
+
+
+}
+maketea() */
+
+/* function greeth(name,desigantion){
+
+  console.log("Good morining"+desigantion+""+name)
+  console.log("have nice day")
+
+}
+
+greeth("John","Mr")
+greeth("John","Mrs")
+greeth("John","Miss")
+ */
+
+//function use return 
+/* function getperime(radius){
+
+
+    return(2*3.14*radius)
+}
+
+
+ let p = getperime(20)
+ let p2 = getperime(30)
+
+ console.log(p)
+ console.log(p2)
+
+//function use console.log
+
+ function getperime(radius){
+
+
+    console.log(2*3.14*radius)
+}
+
+
+ let p11 = getperime(20)
+ let p21 = getperime(30)
+ */
+
+
+
+//json
+/* let student = {
+ details : {
+     name:"John",
+  age:20,
+  city:"Kandy"
+ },    
+  
+ subjects : {
+
+    maths:90,
+    science:80,
+    english:70
+
+  }
+
+
+
+}
+console.log(student.details.name)
+console.log(student.subjects.maths)
+console.log(student.subjects.science)
+console.log(student.subjects.english) */
